3c26a11080fd8e49a97a5ea115433b663e930131
avkotau <kotov.alexander1@mail.ru>
2023-05-11 17:42:29 +0200

    added new src

--- src1/App.tsx
+++ src1/App.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import ButtonItem from '../src/02-button/ButtonItem'
-import TaskUseState from '../src/03-useState/TaskUseState'
-import TaskFilter from '../src/04-filter/TaskFilter'
-import Map from '../src/01-map/Map'
+import ButtonItem from './/02-button/ButtonItem'
+import TaskUseState from './/03-useState/TaskUseState'
+import TaskFilter from './/04-filter/TaskFilter'
+import Map from './/01-map/Map'
 import FullContainer from "./05-input/FullContainer";
 
 
